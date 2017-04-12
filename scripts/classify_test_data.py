028dde29c4f97209aef783f40eb63502cba7d38a
Michael Meli <mjmeli94@gmail.com>
2017-04-12 13:57:42 -0400

    script to classify test data

---
+++ scripts/classify_test_data.py
@@ -0,0 +1,46 @@
+"""
+    classify_test_data.py
+    Performs classification on the test data, pulling the positive classes.
+"""
+import sys
+import glob
+from modules import config, data, helpers
+from modules import classifier as classifierlib
+
+# Data folder - should already exist
+DATA_FOLDER_PATH = config.get_test_data_folder_path()
+
+# Read in each file in the test set
+print("Finding test data...")
+test_data = glob.glob(DATA_FOLDER_PATH + "/*")
+
+# Load the classifier from file
+filepath = config.get_classifier_pickle_file_path("ensemble")
+(classifier, word_features, tag_features, use_core_words) = classifierlib.load_classifier_from_file(filepath)
+
+# Perform classification on each file
+print("Classifying...")
+pos_class = []
+neg_class = []
+for i, td_file in enumerate(test_data):
+    # Print progress
+    sys.stdout.write("\r{}/{}".format(i + 1, len(test_data)))
+    sys.stdout.flush()
+
+    # Convert json string in file to object
+    with open(td_file, 'r') as f:
+        record_json = f.read().strip()
+    record = helpers.json_string_to_object(record_json)
+
+    # Classify
+    classification = classifier.classify_record(record, word_features, tag_features, use_core_words)
+    if classification:
+        pos_class.append(record['id'])
+    else:
+        neg_class.append(record['id'])
+
+# Print out statistics
+print("\nDone!")
+print("  Classified {} files".format(len(test_data)))
+print("  pos: {}".format(len(pos_class)))
+print("  neg: {}".format(len(neg_class)))
